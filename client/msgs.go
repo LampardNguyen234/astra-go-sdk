ea80c4f48566e2bd78a79dd26ea51333bd70b7c0
thanhnv <thanhnv@stellalab.com>
2023-05-26 13:41:50 +0700

    feat: add sdk.msg parser

---
+++ client/msgs.go
@@ -0,0 +1,82 @@
+package client
+
+import (
+	"fmt"
+	"github.com/LampardNguyen234/astra-go-sdk/common"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingTypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	vestingTypes "github.com/evmos/evmos/v6/x/vesting/types"
+)
+
+// ParseCosmosMsgValue returns the value of the given Cosmos message.
+func ParseCosmosMsgValue(msg sdk.Msg) (sdk.Int, error) {
+	v := sdk.ZeroInt()
+	switch msg.(type) {
+	case *bankTypes.MsgSend:
+		v = msg.(*bankTypes.MsgSend).Amount.AmountOf(common.BaseDenom)
+	case *bankTypes.MsgMultiSend:
+		tmpMsg := msg.(*bankTypes.MsgMultiSend)
+		for _, out := range tmpMsg.Outputs {
+			v = v.Add(out.Coins.AmountOf(common.BaseDenom))
+		}
+
+	case *stakingTypes.MsgDelegate:
+		v = msg.(*stakingTypes.MsgDelegate).Amount.Amount
+	case *stakingTypes.MsgBeginRedelegate:
+		v = msg.(*stakingTypes.MsgBeginRedelegate).Amount.Amount
+	case *stakingTypes.MsgUndelegate:
+		v = msg.(*stakingTypes.MsgUndelegate).Amount.Amount
+	case *stakingTypes.MsgCreateValidator:
+		v = msg.(*stakingTypes.MsgCreateValidator).Value.Amount
+
+	case *vestingTypes.MsgCreateClawbackVestingAccount:
+		tmpMsg := msg.(*vestingTypes.MsgCreateClawbackVestingAccount)
+		for _, period := range tmpMsg.VestingPeriods {
+			v = v.Add(period.Amount.AmountOf(common.BaseDenom))
+		}
+	default:
+		return sdk.ZeroInt(), fmt.Errorf("msg `%v` not supported", sdk.MsgTypeURL(msg))
+	}
+
+	return v, nil
+}
+
+// ParseCosmosMsgSender returns the sender of the given Cosmos message.
+func ParseCosmosMsgSender(msg sdk.Msg) (string, error) {
+	ret := ""
+	switch msg.(type) {
+	case *bankTypes.MsgSend,
+		*stakingTypes.MsgCreateValidator,
+		*stakingTypes.MsgDelegate,
+		*stakingTypes.MsgBeginRedelegate,
+		*stakingTypes.MsgUndelegate,
+		*vestingTypes.MsgCreateClawbackVestingAccount,
+		*vestingTypes.MsgClawback:
+		ret = msg.GetSigners()[0].String()
+	default:
+		return "", fmt.Errorf("msg `%v` not supported", sdk.MsgTypeURL(msg))
+	}
+
+	return ret, nil
+}
+
+// ParseCosmosMsgReceiver returns the recipients of the given Cosmos message.
+func ParseCosmosMsgReceiver(msg sdk.Msg) (string, error) {
+	ret := ""
+	switch msg.(type) {
+	case *bankTypes.MsgSend:
+		ret = msg.(*bankTypes.MsgSend).ToAddress
+	case *vestingTypes.MsgCreateClawbackVestingAccount:
+		ret = msg.(*vestingTypes.MsgCreateClawbackVestingAccount).ToAddress
+	case *stakingTypes.MsgCreateValidator,
+		*stakingTypes.MsgDelegate,
+		*stakingTypes.MsgBeginRedelegate,
+		*stakingTypes.MsgUndelegate,
+		*vestingTypes.MsgClawback:
+	default:
+		return "", fmt.Errorf("msg `%v` not supported", sdk.MsgTypeURL(msg))
+	}
+
+	return ret, nil
+}
